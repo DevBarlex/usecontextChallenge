5cd6335f17de2e41e8c25cc7afd88233b6157c25
DevBarlex <alexispaniagua1@gmail.com>
2025-03-19 02:34:47 +0100

    Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import './App.css'; // Archivo de estilos CSS
+import { ThemeProvider, useTheme } from './themes/ThemeContext';
+import RoutesApp from './routes/RoutesApp';
+import './App.css';
 
 const App = () => {
   return (
-<></>
+    <ThemeProvider>
+      <ThemedApp />
+    </ThemeProvider>
+  );
+};
+
+const ThemedApp = () => {
+  const { theme } = useTheme(); 
+
+  return (
+    <section className={`App ${theme}`}>
+      <RoutesApp />
+    </section>
   );
 };
 
